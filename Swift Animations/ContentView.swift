765fe1b00699dfd2fc58fb7d6a3a51b9c5df694e
Sambuxc <samwbru@gmail.com>
2025-12-07 08:33:14 +0000

    Initial Commit

---
+++ Swift Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift Animations
+//
+//  Created by Samuel Bruton on 07/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift Animations/Swift_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AnimationsApp.swift
+//  Swift Animations
+//
+//  Created by Samuel Bruton on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
